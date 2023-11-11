1366f539f3625e091b293d7a7e572a7f8c0ec4d9
Otavio Augusto <otavioscr42@gmail.com>
2023-11-11 17:08:22 -0300

    update

--- desafioJS.ts
+++ desafioJS.ts
@@ -21,7 +21,7 @@ class Library {
         this.books = [];
     }
 
-    function addBook(bookInfo: Omit<Book, 'id'>){
+    addBook(bookInfo: Omit<Book, 'id'>){
         const newBook = new Book(
             this.generateBookID(),
             bookInfo.title,
@@ -31,10 +31,54 @@ class Library {
         return bookInfo
     }
 
-    function generateBookID(){
+    generateBookID(){
         const random = (num: any) => Math.floor(Math.random() * num);
-        return random(100000)
+        return random(100000).toString()
     }
+
+    removeBookByID(id: string): void{
+        const index = this.books.findIndex(book => book.id === id);
+
+        if (index !== -1) {
+            this.books.splice(index, 1);
+        }
+    }
+
+    getBooks(): Book[]{
+        return this.books
+    }
+
+    getBookById(id: string): Book | undefined{
+        return this.books.find(book => book.id === id)
+    }
+
+    updateBookById(id: string, info: { title?: string, description?: string, author?: string }): Book | undefined{
+        const bookToUpdate = this.getBookById(id);
+
+        if (bookToUpdate){
+
+            if (info.title){
+                bookToUpdate.title = info.title;
+            }
+            if (info.description){
+                bookToUpdate.description = info.description;
+            }
+            if (info.author){
+                bookToUpdate.author = info.author;
+            }
+            
+            return bookToUpdate;
+        }
+
+    }
+
 }
 
-//const newBook = new Book(); 
+const library = new Library();
+
+const addedBook = library.addBook({
+    title: "Example Title",
+    description: "Example Description",
+    author: "Example Author"
+});
+
